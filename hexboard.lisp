01994a3849b183e982b5b9d1da9d8e153eaf3ba1
justinhj <justinhj@8980f699-3b1e-0410-a94e-9fc39004bd68>
2007-02-24 22:10:40 +0000

    tightened looseness

--- hexboard.lisp
+++ hexboard.lisp
@@ -19,17 +19,23 @@
 		    (if (< index (length (aref rings ring)))
 			(hb-set-ring-index object ring index t))))))
 
+(defparameter *debug-hb-draw* nil)
+
 (defun hb-draw-ring-index(hb ring index ring-items)
   (with-slots (cx cy ring-radius) hb
-	      (let* ((frac (/ index ring-items))
+	      (let* ((frac (* PI 2.0 (/ index ring-items)))
 		     (x (* (sin frac) ring ring-radius))
-		     (y (* (cos frac) ring ring-radius))
+		     (y (* (cos frac) ring ring-radius -1.0))
 		     (val (hb-get-ring-index hb ring index)))
-		(sdl:draw-box (sdl:rectangle :x (+ cx x) :y (+ cy y) :w 2 :h 2)
-			      :color (if val 
-					 (sdl:color :r 255 :g 0 :b 0) 
-				       (sdl:color :r 0 :g 255 :b 0)) 
-			      :surface sdl:*default-display*))))
+		(if *debug-hb-draw*
+		    (sdl:draw-string-centered-* (format nil "r: ~a i: ~a." ring index)
+						(+ cx x) (+ cy y)
+						:surface sdl:*default-display*)
+		  (sdl:draw-box (sdl:rectangle :x (+ cx x) :y (+ cy y) :w 2 :h 2)
+				:color (if val 
+					   (sdl:color :r 255 :g 0 :b 0) 
+					 (sdl:color :r 0 :g 255 :b 0)) 
+				:surface sdl:*default-display*)))))
 
 (defmethod draw ((object hex-board))
   (with-slots (cx cy num-rings rings) object
@@ -40,13 +46,13 @@
 			    (hb-draw-ring-index object ring index num-items))))))
 
 
-(defun make-hex-board(rings radius x y)
+(defun make-hex-board(rings sides radius x y)
   (let ((board (make-instance 'hex-board :num-rings rings :ring-radius radius
 			      :rings (make-array rings :adjustable nil)
 			      :cx x :cy y)))
     (loop for r from 0 to (1- rings) do
 	  (setf (aref (slot-value board 'rings) r)
-		(make-array (1+ r) :adjustable nil :initial-element nil)))
+		(make-array (1+ (* sides r)) :adjustable nil :initial-element nil)))
     board))
 
 (defun hb-screen-x-y-to-ring(hb x y)
@@ -55,12 +61,15 @@
 		(round (/ dist ring-radius)))))
   
 (defun hb-screen-x-y-to-ring-index(hb x y)
-  (with-slots (cx cy ring-radius num-rings) hb
+  (with-slots (cx cy ring-radius num-rings rings) hb
 	      (let ((dx (- x cx))
-		    (dy (- y cy)))
-		(let ((angle-degs (rads-degs (atan2 dx (- dy)))))
-		  (round (/ angle-degs num-rings)))))) ; now return the number elements in ring index
-					; round divided by the angle
+		    (dy (- y cy))
+		    (ring (hb-screen-x-y-to-ring hb x y)))
+		(if (< ring num-rings)
+		    (let ((angle-degs (rads-degs (atan2 dx (- dy))))
+			  (items (length (aref rings ring))))
+		      (round (/ angle-degs items)))
+		  -1))))
 
 ;;;; todo for these two functions, validate the ring index to the vertex length
 
