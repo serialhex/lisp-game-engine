88df3663d3dd0b096a08f265cc9d93ecc58f07df
justinhj <justinhj@8980f699-3b1e-0410-a94e-9fc39004bd68>
2007-02-26 23:55:07 +0000

    nearly there

--- trunk/hexgame.lisp
+++ trunk/hexgame.lisp
@@ -1,11 +1,22 @@
 ;;;; Hex tetris game experiment
 ; (C)2007 Justin Heyes-Jones
 
+; TODO
+; look at distance increase/decrease per ring and rendering 
+; changes to make it clearer
+; full ring elimination
+; shape offsets and proper tetris style shapes
+; gameplay alternatives
+; engine: timer and input events
+
 (in-package #:cl-user)  
 
 ; window or screen height TODO this must go somewhere better
 (defparameter *WINDOW-WIDTH* 640)
 (defparameter *WINDOW-HEIGHT* 480)
+
+(defparameter *mouse-click-x* -1)
+(defparameter *mouse-click-y* -1)
 
 ;;;; UTIL
 
@@ -21,6 +32,19 @@
 
 ;;;; DATA 
 
+; the exe for sbcl needs to run this
+#+sbcl (defun main1()
+ (cffi:define-foreign-library sdl
+                              (t (:default "SDL")))
+ (cffi:use-foreign-library sdl)
+ (sdl:with-init ()
+                (hexgame))
+ (sb-ext:quit))
+
+; This lets you make an exe for sbcl
+#+sbcl (defun make-exe()
+  (sb-ext:save-lisp-and-die "hexgame.exe" :purify t :toplevel #'main1 :executable t))
+
 ; a list of game objects
 ; todo this should be part of the engine maybe?
 (defparameter *game-objects* nil)
@@ -28,9 +52,6 @@
 (defun show-frame-rate()
   (sdl:draw-string-centered-* (format nil "fps: ~a" (sdl:frame-rate)) (screen-center-x) (screen-center-y)
 					:surface sdl:*default-display*))
-
-(defparameter *mouse-click-x* -1)
-(defparameter *mouse-click-y* -1)
 
 (defun show-debug-info()
   (let ((hb (car *game-objects*)))
@@ -50,7 +71,7 @@
 ; todo gameobjects
 (defun init-game-objects()
   ; add game objects to update loop
-  (add-object (make-hex-board 15 6 15 (screen-center-x) (screen-center-y)))
+  (add-object (make-hex-board 15 30 15 (screen-center-x) (screen-center-y)))
 )
 
 (defun update-game-objects()
@@ -64,7 +85,7 @@
 (defun hexgame()
   "hex game"
   (sdl:with-init ()			;Initialise SDL
-      (setf (sdl:frame-rate) 30) ; Set target framerate (or 0 for unlimited)
+      (setf (sdl:frame-rate) 60) ; Set target framerate (or 0 for unlimited)
       (sdl:window *WINDOW-WIDTH* *WINDOW-HEIGHT* :title-caption "Hex" :icon-caption "Hex")
       (progn
 	;; init your game
@@ -83,7 +104,7 @@
 		 (sdl:clear-display (sdl:color :r #x00 :g #x00 :b #x00))
 		 ;; Do stuff
 ;		 (show-frame-rate)
-		 (show-debug-info)
+		 ;(show-debug-info)
 		 (update-game-objects)
 		 (draw-game-objects)
 		 ;; Update the whole screen 
