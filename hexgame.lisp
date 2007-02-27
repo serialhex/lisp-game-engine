f67629619b16894119df8786d67da5523f21d33a
justinhj <justinhj@8980f699-3b1e-0410-a94e-9fc39004bd68>
2007-02-27 06:37:03 +0000

    advancery +10

--- hexgame.lisp
+++ hexgame.lisp
@@ -1,4 +1,4 @@
-;;;; Hex tetris game experiment
+;;;; Game experiment of a falling block nature
 ; (C)2007 Justin Heyes-Jones
 
 ; TODO
@@ -7,6 +7,11 @@
 ; full ring elimination
 ; shape offsets and proper tetris style shapes
 ; gameplay alternatives
+;    ok
+;    how about, progress bar tells you when you've dumped enough shapes
+;    you have an active shape
+;    right clicking on the board dumps the active shape there
+;    when it lands you can do another one
 ; engine: timer and input events
 
 (in-package #:cl-user)  
@@ -28,7 +33,7 @@
 ;;;; GAME 
 (load "gameobjects.lisp") ; generic game objects
 
-(load "hexboard.lisp") ; managing the data and drawing of a hex board
+(load "hexboard.lisp") ; managing the data and drawing of the board
 
 ;;;; DATA 
 
@@ -38,12 +43,12 @@
                               (t (:default "SDL")))
  (cffi:use-foreign-library sdl)
  (sdl:with-init ()
-                (hexgame))
+                (game))
  (sb-ext:quit))
 
 ; This lets you make an exe for sbcl
 #+sbcl (defun make-exe()
-  (sb-ext:save-lisp-and-die "hexgame.exe" :purify t :toplevel #'main1 :executable t))
+  (sb-ext:save-lisp-and-die "game.exe" :purify t :toplevel #'main1 :executable t))
 
 ; a list of game objects
 ; todo this should be part of the engine maybe?
@@ -71,7 +76,7 @@
 ; todo gameobjects
 (defun init-game-objects()
   ; add game objects to update loop
-  (add-object (make-hex-board 15 30 15 (screen-center-x) (screen-center-y)))
+  (add-object (make-game-board 15 30 15 (screen-center-x) (screen-center-y)))
 )
 
 (defun update-game-objects()
@@ -82,11 +87,11 @@
   (loop for game-object in *game-objects* do
 	(draw game-object)))
 
-(defun hexgame()
-  "hex game"
-  (sdl:with-init ()			;Initialise SDL
-      (setf (sdl:frame-rate) 60) ; Set target framerate (or 0 for unlimited)
-      (sdl:window *WINDOW-WIDTH* *WINDOW-HEIGHT* :title-caption "Hex" :icon-caption "Hex")
+(defun game()
+  "A game"
+  (sdl:with-init () ;Initialise SDL
+      (setf (sdl:frame-rate) 2) ; Set target framerate (or 0 for unlimited)
+      (sdl:window *WINDOW-WIDTH* *WINDOW-HEIGHT* :title-caption "A Game" :icon-caption "A Game")
       (progn
 	;; init your game
 	(sdl:initialise-default-font)
